54eb2aa6fb4dc437578e5ec9b4250aab2e3a7ab2
Arash Tadjiki <atadjiki@yahoo.com>
2016-06-13 21:53:01 -0600

    Add files via upload

--- CC/Assets/Scripts/PlayerController.cs
+++ CC/Assets/Scripts/PlayerController.cs
@@ -1,160 +1,238 @@
-﻿using UnityEngine;
-using System.Collections;
-using Prime31;
+﻿/*
+* Author: Arash Tadjiki
+* Date - 12/6/2016
+* 
+* Prototype Build:
+* This script, based on the tutorial controller, 
+* controls the core mechanics of the game, allowing the 
+* player to move in either infinite runner mode
+* or in normal platformer mode. Several public variables
+* have been added: a run multiplier, and booleans
+* for toggling infinite runner mode and walking.
+*/
+using UnityEngine;
 using UnityEngine.UI;
-using System;
+using Prime31;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerBehavior : MonoBehaviour 
+{
 
-	public GameObject gameCamera;
-    public GameObject gameOverPanel;
-
-	public float jumpHeight = 2;
-	public float walkSpeed = 3;
 	public float gravity = -35;
-
+	public float jumpHeight = 10;
+	public float walkSpeed = 3;
+	public float friction = 0.96f;
 	public int health = 100;
+	public int runMultiplier = 2;
+	public bool infiniteRunnerMode = true;
+	public bool enableWalking = true;
+
+	public GameObject startCheckpoint;
+	public GameObject healthBar;
+	public GameObject gameCamera;
+	public GameObject gameOverPanel;
 
 	private CharacterController2D _controller;
-    //private AnimationController2D _animator;
-    //Playzone is the screen area, the area the player cannot exit.
-    private Vector3 playZone;
-
-	private int currentHealth = 0;
+	private AnimationController2D _animator;
+	private int _currentHealth = 0;
 	private bool playerControl = true;
-    private float half;
+
+
+
+
 	// Use this for initialization
-	void Start () {
-        //initialize _controller component
-		_controller = gameObject.GetComponent<CharacterController2D>();
-        //playZone initialization
-        playZone = gameCamera.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, 0.0f));
-        //Half is the the middle x point of the sprite
-        half = gameObject.GetComponent<Renderer>().bounds.size.x / 2;
-        //_animator = gameObject.GetComponent<AnimationController2D>();
+	void Start () 
+	{
 
-        //gameCamera.GetComponent<CameraFollow2D>().startCameraFollow(this.gameObject);
+		if (!(ReferenceEquals (startCheckpoint, null)))
+			transform.position = startCheckpoint.transform.position;
 
-        currentHealth = health;
+		_controller = gameObject.GetComponent<CharacterController2D> ();
+		_animator = gameObject.GetComponent<AnimationController2D> ();
+		_currentHealth = health;
+
+		gameCamera.GetComponent<CameraFollow2D> ().startCameraFollow (this.gameObject);
+		
+
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update () 
 	{
-        playZone = Camera.main.WorldToViewportPoint(transform.position);
-        playZone.x = Mathf.Clamp(playZone.x, 0.05f, 0.95f);
-        playZone.y = Mathf.Clamp01(playZone.y);
-        transform.position = Camera.main.ViewportToWorldPoint(playZone);
-        if (playerControl)
+		if (playerControl == true) 
 		{
-			Vector3 velocity = PlayerInput();
-			_controller.move(velocity * Time.deltaTime);
+			_controller.move (playerInput () * Time.deltaTime);
 		}
 
 	}
 
-    private Vector3 PlayerInput()
+	public Vector3 playerInput()
 	{
 		Vector3 velocity = _controller.velocity;
 
-		if (_controller.isGrounded && _controller.ground != null && _controller.ground.tag == "MovingPlatform")
+		//check if player is standing on moving platform
+		CheckMovingPlatform();
+
+		//if false, allow the player to move normally
+		if (infiniteRunnerMode == false) 
 		{
-			this.transform.parent = _controller.ground.transform;
-		}
-		else
-		{
-			if (this.transform.parent != null)
+			velocity.x = 0;
+
+			//directional inputs
+			if (Input.GetAxis ("Horizontal") < 0) 
 			{
-				transform.parent = null;
+
+				if (enableWalking) 
+				{
+					if (_controller.isGrounded && Input.GetKey (KeyCode.LeftShift)) 
+					{
+						velocity.x = -walkSpeed * runMultiplier;
+						_animator.setAnimation ("RunPlayer");
+					} 
+					else if (_controller.isGrounded) 
+					{
+						velocity.x = -walkSpeed;
+						_animator.setAnimation ("WalkPlayer");
+					}
+				} else 
+				{
+					velocity.x = -walkSpeed * runMultiplier;
+					_animator.setAnimation ("RunPlayer");
+				}
+
+				_animator.setFacing ("Left");
+
+			} 
+			else if (Input.GetAxis ("Horizontal") > 0) 
+			{
+
+				if (enableWalking) {
+					if (_controller.isGrounded && Input.GetKey (KeyCode.LeftShift)) {
+						velocity.x = walkSpeed * runMultiplier;
+						_animator.setAnimation ("RunPlayer");
+					} else if (_controller.isGrounded) {
+						velocity.x = walkSpeed;
+						_animator.setAnimation ("WalkPlayer");
+					}
+				} else 
+				{
+					velocity.x = walkSpeed * runMultiplier;
+					_animator.setAnimation ("RunPlayer");
+				}
+
+				_animator.setFacing ("Right");
+
+			} 
+			else 
+			{
+				_animator.setAnimation("IdlePlayer");
 			}
 		}
-
-		if (Input.GetAxis("Horizontal") < 0)
+		else //if infinite mode is active:
 		{
-                      
-            velocity.x = walkSpeed * -1;
-            if (_controller.isGrounded)
-            {
-                //Play walking anim
-                //_animator.setAnimation("RUN");
-            } 
-            //_animator.setFacing("Left");
-            	
-
-		}
-		else if (Input.GetAxis("Horizontal") > 0)
-		{
-			velocity.x = walkSpeed;
-			if (_controller.isGrounded)
+			
+			if (enableWalking) {
+				if (_controller.isGrounded && Input.GetKey (KeyCode.LeftShift)) {
+					velocity.x = walkSpeed * runMultiplier;
+					_animator.setAnimation ("RunPlayer");
+				} else if (_controller.isGrounded) {
+					velocity.x = walkSpeed;
+					_animator.setAnimation ("WalkPlayer");
+				}
+			} else 
 			{
-				//Play walking anim
-				//_animator.setAnimation("RUN");
+				velocity.x = walkSpeed * runMultiplier;
+				_animator.setAnimation ("RunPlayer");
 			}
-			//_animator.setFacing("Right");
-		}
-		else
-		{
-			//play idle anim
-			//_animator.setAnimation("IDLE");
+
+			_animator.setFacing ("Right");
 		}
 
-		if (Input.GetAxis("Jump") > 0 && _controller.isGrounded)
+
+
+
+		if (Input.GetAxis ("Jump") > 0 && _controller.isGrounded) 
 		{
-			velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
-			//play jump anim
-			//_animator.setAnimation("JUMP");
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				velocity.y = Mathf.Sqrt (4f * jumpHeight * -gravity);	
+			} 
+			else 
+			{
+				//Debug.log ("Space bar pressed);
+				velocity.y = Mathf.Sqrt (2f * jumpHeight * -gravity);
+			}
+
+			//play jump animation
+			_animator.setAnimation("JumpPlayer");
 		}
 
-		velocity.x *= .92f;
+		velocity.x *= friction;
 
 		velocity.y += gravity * Time.deltaTime;
+		gameObject.GetComponent<CharacterController2D>().move(velocity * Time.deltaTime);
 
 		return velocity;
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		//Check what we're hitting.
-		if (col.tag == "KillZ")
-		{
-			PlayerFallDeath();
+
+		if (col.tag == "KillZ") {
+			PlayerFallDeath ();
+		} else if (col.tag == "Damaging") {
+			PlayerDamage (30);
+		} else if (col.tag == "Finish") {
+			Application.LoadLevel (0);
+		} else if (col.tag == "Checkpoint") {
+
+			//attaches player to the most recently
+			//touched checkpoint
+			Debug.Log(col.gameObject.name);
+			startCheckpoint = col.gameObject;
+	
 		}
-		else if(col.tag == "Damaging")
-		{
-			PlayerDamage(10);
-		}
-	}
-
-	private void PlayerDamage (int damage)
-	{
-		currentHealth -= damage;
-
-		float normalizedHealth = (float)currentHealth / (float)health;
-
-		//healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(normalizedHealth*256,32);
-
-		if (currentHealth <= 0)
-		{
-			currentHealth = 0;
-			PlayerDeath();
-		}
-	}
-
-	private void PlayerDeath()
-	{
-		//_animator.setAnimation("DEATH");
-		playerControl = false;
-        gameOverPanel.SetActive(true);
 	}
 
 	private void PlayerFallDeath()
 	{
-		currentHealth = 0;
-		//healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 32);
-		Debug.Log(currentHealth);
+		
+		_currentHealth = 0;
+		float normalizedHealth = (float) _currentHealth / (float) health;
+		healthBar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (normalizedHealth * 256, 32);
+		gameCamera.GetComponent<CameraFollow2D> ().stopCameraFollow();
 
-		//gameCamera.GetComponent<CameraFollow2D>().stopCameraFollow();
+		gameOverPanel.SetActive (true);
+	}
 
-        gameOverPanel.SetActive(true);
-    }
+	private void PlayerDamage(int damage)
+	{
+		
+		_currentHealth -= damage;
+		float normalizedHealth = (float) _currentHealth / (float) health;
+		healthBar.GetComponent<RectTransform> ().sizeDelta = new Vector2 (normalizedHealth * 256, 32);
+
+		if (_currentHealth <= 0) 
+		{
+			PlayerDeath ();
+		}
+
+	}
+
+	private void PlayerDeath()
+	{
+		playerControl = false;
+		_animator.setAnimation ("DeathPlayer");
+
+		gameOverPanel.SetActive (true);
+	}
+
+	private void CheckMovingPlatform()
+	{
+		if (_controller.isGrounded && _controller.ground != null && _controller.ground.tag == "Moving Platform")
+			this.transform.parent = _controller.ground.transform;
+		else {
+			if (this.transform.parent != null)
+				transform.parent = null;
+		}
+	}
 }
