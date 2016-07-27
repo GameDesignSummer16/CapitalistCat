b6f721f65815324103e01129f24f0839fb88362e
u0728671 <u0728671@1110-mac-20.lib.utah.edu>
2016-07-26 18:57:01 -0600

    Uploading Beta

---
+++ CC/Beta/CC/Assets/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+
+	public GameObject endPosition;
+	public bool destroyOnTrigger;
+	public bool plummetOnTrigger;
+	public bool reverseOnTrigger;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public Transform getEndTransform()
+	{
+		return endPosition.transform;
+	}
+
+
+}
